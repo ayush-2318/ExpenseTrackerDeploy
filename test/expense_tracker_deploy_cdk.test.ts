bcee24e6d9813024b3c828420b052603dad57728
Ayush Kumar <golutpr@gmail.com>
2025-05-17 22:15:50 +0530

    Initial commit

---
+++ test/expense_tracker_deploy_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExpenseTrackerDeployCdk from '../lib/expense_tracker_deploy_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/expense_tracker_deploy_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExpenseTrackerDeployCdk.ExpenseTrackerDeployCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
